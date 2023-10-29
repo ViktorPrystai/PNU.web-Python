f397646b56786fc311427e1aa3db620a63656c86
ViktorPrystai <viktor.prystai.21@pnu.edu.ua>
2023-10-29 13:13:01 +0200

    lab3

---
+++ data.py
@@ -0,0 +1,20 @@
+posts = [
+    {
+        'title': 'CSS and HTML',
+        'body': 'Lorem ipsum dolor sit amet.',
+        'exp': '2 year',
+        'date_ms': 1593607500000,
+    },
+    {
+        'title': 'C# .NET',
+        'body': 'Lorem ipsum dolor sit amet.',
+        'exp': '1,5 year',
+        'date_ms': 1593870300000,
+    },
+    {
+        'title': 'Angular and js',
+        'body': 'Lorem ipsum dolor sit amet.',
+        'exp': '5 month',
+        'date_ms': 1594419000000,
+    }
+]
